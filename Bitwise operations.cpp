73d89477f3b8fc9a4c6a259fb560e0201d6c0231
vijayassrao <vijayassrao@gmail.com>
2022-09-11 23:13:37 +0530

    Decimal to Binary Number Conversion

---
+++ Bitwise operations.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    for (int  i= 1; i<=n; i++)
+    {
+        cout<<i<<endl;
+    }
+    for(int a=0, b=1;a>=0 && b>=1; a--,b--){
+        cout<<a<<" "<<b<<endl;
+    }
+
+    
+}
